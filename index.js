a9a9bd2182515f902445acadb92d0ae60d05e32d
Austin <austinpulley19@yahoo.com>
2022-02-16 18:51:16 -0600

    commit tree f2cec7e5b9256251ddc438b86832a260fdb85c02

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+function superbowlWin(record) {
+    const yearWon = record.find( record => record.result === "W")
+    if(yearWon && yearWon.result === "W") {
+        return yearWon.year;
+    }
+    else {
+       return undefined
+    }
+}
